33298de7da4a59f17e71a5173c825d370b06890b
suveer <suveer@gmail.com>
2018-01-04 13:21:45 +0000

    made some changes

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var http = require('http');
 var os = require('os');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('Hello World!!')
+    res.write('Hollaa World!!')
 }).listen(8080);
