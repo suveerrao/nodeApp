14637c7de95ba2758474924d8e3194e2e4cfdd3a
root <root@devmachine.c.tg-k8s.internal>
2018-01-04 12:51:50 +0000

    Fresh commit

---
+++ index.js
@@ -0,0 +1,6 @@
+var http = require('http');
+var os = require('os');
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('Hello World!!')
+}).listen(8080);
